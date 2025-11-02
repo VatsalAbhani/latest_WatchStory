5ca1a86f6dd0fbd605bbe6d0dcc45303218ede22
VatsalAbhani <vatsalabhani@gmail.com>
2025-11-02 19:34:11 +0400

    update 4

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -448,7 +448,7 @@ loadJournalPosts();
             A Story on Your Wrist
           </h2> */}
 
-          <h1 className="text-center font-sans text-lg sm:text-2xl lg:text-4xl leading-tight z-30">
+          <h1 className="text-center font-sans text-2xl sm:text-2xl lg:text-4xl leading-tight z-30">
   More Than Time,<br />
   A Story on Your Wrist
     </h1>
